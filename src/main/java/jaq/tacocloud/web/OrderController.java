21fcdc08a65b8d304dff0f8e34400f786e5114ef
Jazi <gleb_andreev01@mail.ru>
2025-09-07 00:12:09 +0300

    Added taco order form with validation and error handling, implemented controller for GET and POST requests, integrated Thymeleaf template and styles.

---
+++ src/main/java/jaq/tacocloud/web/OrderController.java
@@ -0,0 +1,35 @@
+package jaq.tacocloud.web;
+
+import jakarta.validation.Valid;
+import jaq.tacocloud.model.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+public class OrderController {
+
+    @ModelAttribute("order")
+    public Order order() {
+        return new Order();
+    }
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus) {
+        if (errors.hasErrors())
+            return "orderForm";
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
+}
