2502a09fb372042aab9f6348cecf74fbee974c21
Jazi <gleb_andreev01@mail.ru>
2025-09-16 18:28:53 +0300

    add spring-boot-starter-data-rest for automatic REST API exposure

--- src/main/java/jaq/tacocloud/data/TacoRepo.java
+++ src/main/java/jaq/tacocloud/data/TacoRepo.java
@@ -2,7 +2,7 @@ package jaq.tacocloud.data;
 
 import jaq.tacocloud.Taco;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TacoRepo extends CrudRepository<Taco, Long>{
-
+public interface TacoRepo extends CrudRepository<Taco, Long>, PagingAndSortingRepository<Taco,Long> {
 }
