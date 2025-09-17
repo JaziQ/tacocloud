665d56708a09a23c47eb64760e449477ddd1f4e3
Jazi <gleb_andreev01@mail.ru>
2025-09-17 14:32:44 +0300

    Associate orders with users

--- src/main/java/jaq/tacocloud/web/OrderController.java
+++ src/main/java/jaq/tacocloud/web/OrderController.java
@@ -1,9 +1,12 @@
 package jaq.tacocloud.web;
 
 import jakarta.validation.Valid;
+import org.springframework.ui.Model;
+import jaq.tacocloud.User;
 import jaq.tacocloud.data.OrderRepo;
 import jaq.tacocloud.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -25,10 +28,21 @@ public class OrderController {
     public Order order() {
         return new Order();
     }
+
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(Model model, @AuthenticationPrincipal User user) {
+        Order order = (Order) model.getAttribute("order");
+        if (order == null || order.getTacos().isEmpty()) {
+            return "redirect:/design";
+        }
+        order.setUser(user);
+        order.setDeliveryStreet(order.getDeliveryStreet());
+        order.setDeliveryCity(order.getDeliveryCity());
+        order.setDeliveryState(order.getDeliveryState());
+        order.setDeliveryZip(order.getDeliveryZip());
         return "orderForm";
     }
+
     @PostMapping
     public String processOrder(@Valid Order order, Errors errors,
                                SessionStatus sessionStatus) {
